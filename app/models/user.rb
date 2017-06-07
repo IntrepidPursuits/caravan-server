b2b2680249d60b5bab7515b3266b94d00e5d9fb0
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-07 16:10:04 -0400

    write and test associations & validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  has_many :trips, as: :creator
-
-  has_many :user_trips
-  has_many :trips, through: :user_trips
-
-  has_many :seats
   has_many :cars, through: :seats
+  has_many :seats
+  has_many :trips, as: :creator
+  has_many :trips, through: :user_trips
+  has_many :user_trips
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,14 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "associations" do
-    
+    it { should have_many(:cars).through(:seats) }
+    it { should have_many(:seats) }
+    it { should have_many(:trips) }
+    it { should have_many(:trips).through(:user_trips) }
+    it { should have_many(:user_trips) }
   end
 
   describe "validations" do
-
+    
   end
 end
