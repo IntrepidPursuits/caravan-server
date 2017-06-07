9b7e3c72bf2ecabadd30f5a334ab1f238ad56568
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-07 11:02:13 -0400

    add associations and validations for cars and users

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,14 @@
 class Car < ApplicationRecord
+  belongs_to :trip
+  has_many :seats
+  has_many :users, through: :seats
+
+  enum status: {
+    "Not started": 0,
+    "In transit": 1,
+    "Arrived": 2
+  }
+
+  validates :status, inclusion: { in: statuses.keys }
+  validates :num_seats, range: [1..25]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :trips, as: :creator
+
+  has_many :user_trips
+  has_many :trips, through: :user_trips
+
+  has_many :seats
+  has_many :cars, through: :seats
 end
