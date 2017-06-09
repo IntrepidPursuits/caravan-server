cdd6a550b4a248f5b031d5e5305d621a8280b3bb
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-09 17:26:27 -0400

    create and test Locations

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :location do
-    
+    car
+    latitude 3.492
+    longitude 4.103
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:car) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:latitude) }
+    it { should validate_presence_of(:longitude) }
+  end
 end
