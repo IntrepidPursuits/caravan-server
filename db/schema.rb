dd97a0e415b1817f09ba973d3b3ca263b17e7657
Marjorie Lam <marjorielam@MacBook-Pro.local>
2017-06-12 16:27:49 -0400

    created InviteCodes table, models, and tests, and updated InviteCodeGenerator logic to query InviteCodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616140329) do
+ActiveRecord::Schema.define(version: 20170612193354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
