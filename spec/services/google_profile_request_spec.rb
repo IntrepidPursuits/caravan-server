de390d2ce1ae9a6cc5fb435e3a0bae95ef9b24d9
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-19 14:06:13 -0400

    call the correct stubbing method

--- spec/services/google_profile_request_spec.rb
+++ spec/services/google_profile_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "GoogleProfileRequest" do
   context "for an invalid google uid" do
     it "raise an error" do
       expect do
-        stub_bad_id_request
+        stub_bad_token_request
       end.to raise_error GoogleProfileRequest::InvalidGoogleUser
     end
   end
