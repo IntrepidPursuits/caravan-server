b996fd0e8be3980d60c3bd8b374acfcbcce8e157
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-20 17:08:30 -0400

    restrict routes to only relevant (auths#create)

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
                    value: "application/vnd.caravan-server.com; version=1" },
                    defaults: { format: :json }) do
     resources :auths, only: [:create]
-    resources :users, only: [:create, :show]
-    resources :google_identities, only: [:create]
   end
-  resources :users, only: [:create]
-  resources :google_identities, only: [:create]
 end
