1cee764fefd46f7d2d0684b9d587d6b4dec08200
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-21 16:26:28 -0400

    add route for PATCH /cars/:id

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
                    value: "application/vnd.caravan-server.com; version=1" },
                    defaults: { format: :json }) do
     resources :auths, only: [:create]
-    resources :cars, only: [:create, :show]
+    resources :cars, only: [:create, :show, :update]
     resources :trips, only: [:create, :show]
   end
 end
