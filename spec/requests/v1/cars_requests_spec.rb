643c5cb8a88b67e8333909b2ad2dd974360ce32e
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-22 12:37:30 -0400

    add test for specifying status:arrived as a string

--- spec/requests/v1/cars_requests_spec.rb
+++ spec/requests/v1/cars_requests_spec.rb
@@ -124,36 +124,74 @@ RSpec.describe "Car Requests", type: :request do
 
   describe "PATCH /cars/:id", type: :request do
     context "user can update their car's status to reflect starting the trip" do
-      it "returns updated JSON for the car" do
-        car = create(:car)
+      context "when status is specified as a number" do
+        it "returns updated JSON for the car" do
+          car = create(:car)
 
-        patch(
+          patch(
           api_v1_car_url(car),
           params: { status: 1 }.to_json,
           headers: accept_headers
-        )
+          )
 
-        expect(response).to have_http_status :ok
-        expect(body).to have_json_path("car")
-        expect(body).to have_json_path("car/status")
-        expect(parsed_body["car"]["status"]).to eq("in_transit")
+          expect(response).to have_http_status :ok
+          expect(body).to have_json_path("car")
+          expect(body).to have_json_path("car/status")
+          expect(parsed_body["car"]["status"]).to eq("in_transit")
+        end
+      end
+
+      context "when status is specified as a string" do
+        it "returns updated JSON for the car" do
+          car = create(:car)
+
+          patch(
+          api_v1_car_url(car),
+          params: { status: "in_transit" }.to_json,
+          headers: accept_headers
+          )
+
+          expect(response).to have_http_status :ok
+          expect(body).to have_json_path("car")
+          expect(body).to have_json_path("car/status")
+          expect(parsed_body["car"]["status"]).to eq("in_transit")
+        end
       end
     end
 
     context "user can update car's status to reflect arriving at the destination" do
-      it "returns updated JSON for the car" do
-        car = create(:car)
+      context "when status is specified as a number" do
+        it "returns updated JSON for the car" do
+          car = create(:car)
 
-        patch(
+          patch(
           api_v1_car_url(car),
           params: { status: 2 }.to_json,
           headers: accept_headers
-        )
+          )
 
-        expect(response).to have_http_status :ok
-        expect(body).to have_json_path("car")
-        expect(body).to have_json_path("car/status")
-        expect(parsed_body["car"]["status"]).to eq("arrived")
+          expect(response).to have_http_status :ok
+          expect(body).to have_json_path("car")
+          expect(body).to have_json_path("car/status")
+          expect(parsed_body["car"]["status"]).to eq("arrived")
+        end
+      end
+
+      context "when status is specified as a string" do
+        it "returns updated JSON for the car" do
+          car = create(:car)
+
+          patch(
+            api_v1_car_url(car),
+            params: { status: "arrived" }.to_json,
+            headers: accept_headers
+          )
+
+          expect(response).to have_http_status :ok
+          expect(body).to have_json_path("car")
+          expect(body).to have_json_path("car/status")
+          expect(parsed_body["car"]["status"]).to eq("arrived")
+        end
       end
     end
   end
