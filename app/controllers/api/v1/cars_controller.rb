34ff11853c146741d352a51aba4417ecbd31f147
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-23 17:00:46 -0400

    wrap car creation and auto signup in a transaction

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,25 +1,25 @@
 class Api::V1::CarsController < Api::V1::ApiController
   def create
-    car = Car.create!(car_params)
-    user = current_user
-    Signup.create!(car_id: car.id, trip_id: car.trip_id, user_id: user.id)
-    render json: car,
-      status: :created,
-      serializer: CarSerializer,
-      except: [:car, :cars, :google_identity, :invite_code, :signups]
+    ActiveRecord::Base.transaction do
+      car = Car.create(car_params)
+      user = current_user
+      Signup.create!(car_id: car.id, trip_id: car.trip_id, user_id: user.id)
+    end
+    render json: car, status: :created, serializer: CarSerializer, except: exclusions
   end
 
   def show
     car = Car.find(params[:id])
-    render json: car,
-      status: :ok,
-      serializer: CarSerializer,
-      except: [:car, :cars, :google_identity, :invite_code, :signups]
+    render json: car, status: :ok, serializer: CarSerializer, except: exclusions
   end
 
   private
 
   def car_params
     params.require(:car).permit(:trip_id, :max_seats, :name, :status)
+  end
+
+  def exclusions
+    [:car, :cars, :google_identity, :invite_code, :signups]
   end
 end
