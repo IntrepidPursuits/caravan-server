ea9ae2d66c924513cdf16492295c05f2c3257259
Marjorie Lam <marjorielam@MacBook-Pro.local>
2017-06-27 13:32:16 -0400

    simplify trip tests using RequestExpectations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.include Helpers::Requests, type: :request
-  config.include Helpers::RequestExpectations
+  config.include Helpers::RequestExpectations, type: :request
   config.include Warden::Test::Helpers
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
