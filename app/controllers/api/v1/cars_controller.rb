b2db87c30f5c0a03c138b81ebecc9a2639a9b95f
Riki Konikoff <riki.konikoff@gmail.com>
2017-06-27 22:58:18 -0400

    automatically signup user for a car they create

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,24 +1,26 @@
 class Api::V1::CarsController < Api::V1::ApiController
   def create
-    car = Car.create!(car_params)
-    render json: car,
-      status: :created,
-      serializer: CarSerializer,
-      except: [:car, :cars, :google_identity, :invite_code, :signups]
+    ActiveRecord::Base.transaction do
+      raise ActiveRecord::RecordInvalid unless trip_id = car_params["trip_id"]
+      authorize Trip.find(trip_id), :create_car?
+      @car = CreateACar.perform(car_params, current_user)
+    end
+    render json: @car, status: :created, serializer: CarSerializer, except: exclusions
   end
 
   def show
     car = Car.find(params[:id])
     authorize car
-    render json: car,
-      status: :ok,
-      serializer: CarSerializer,
-      except: [:car, :cars, :google_identity, :invite_code, :signups]
+    render json: car, status: :ok, serializer: CarSerializer, except: exclusions
   end
 
   private
 
   def car_params
     params.require(:car).permit(:trip_id, :max_seats, :name, :status)
+  end
+
+  def exclusions
+    [:car, :cars, :google_identity, :invite_code, :signups]
   end
 end
---
+++ app/policies/trip_policy.rb
@@ -0,0 +1,12 @@
+class TripPolicy < ApplicationPolicy
+  attr_reader :user, :trip
+
+  def initialize(user, trip)
+    @user = user
+    @trip = trip
+  end
+
+  def create_car?
+    trip.users.include?(user)
+  end
+end
